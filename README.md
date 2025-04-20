8fe4eb91239def329cc1f326f379adfa7173f86e
俺不叫化咕龙 <1801943622@qq.com>
2025-04-20 20:57:48 +0800

    Update README with improved ffmpeg commands and adjust main.rs for better frame data handling

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Start
 
-* ffmpeg -i "Wuthering Waves Story Cinematics  The Ending She Desired.mp4" -r 16 -s 240x135 -f image2 ./target/images/%d.jpeg
+* ffmpeg -i "Wuthering Waves Story Cinematics  The Ending She Desired.mp4" -r 16 -s 384x216 -f image2 ./target/images/%d.jpeg
 
 * ffmpeg -i "Wuthering Waves Story Cinematics  The Ending She Desired.mp4" -i "videoplayback.m4a" -c:v copy -c:a aac -shortest output.mp4
--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,23 @@ use std::{
 };
 
 use crossterm::{
-    cursor::MoveTo,
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, LeaveAlternateScreen},
 };
 use image::GenericImageView;
-use rayon::{
-    iter::{IndexedParallelIterator, ParallelIterator},
-    slice::ParallelSliceMut,
-};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 struct FrameData {
     width: u32,
     height: u32,
-    data: Vec<(u8, u8, u8, char)>,
+    //data: Vec<(u8, u8, u8, char)>,
+    data: Vec<u8>,
 }
 
 struct DoubleBuffer {
     front: AtomicPtr<FrameData>,
     back: AtomicPtr<FrameData>,
+    temp: AtomicPtr<FrameData>,
 }
 
 impl DoubleBuffer {
@@ -32,16 +30,22 @@ impl DoubleBuffer {
         let front_box = Box::new(FrameData {
             width,
             height,
-            data: vec![(0, 0, 0, ' '); (width * height) as usize],
+            data: Vec::with_capacity((width * height * 20) as usize),
         });
         let back_box = Box::new(FrameData {
             width,
             height,
-            data: vec![(0, 0, 0, ' '); (width * height) as usize],
+            data: Vec::with_capacity((width * height * 20) as usize),
+        });
+        let temp_box = Box::new(FrameData {
+            width,
+            height,
+            data: Vec::with_capacity((width * height * 20) as usize),
         });
         DoubleBuffer {
             front: AtomicPtr::new(Box::into_raw(front_box)),
             back: AtomicPtr::new(Box::into_raw(back_box)),
+            temp: AtomicPtr::new(Box::into_raw(temp_box)),
         }
     }
 
@@ -57,8 +61,16 @@ impl DoubleBuffer {
     fn front(&self) -> &FrameData {
         unsafe { &*self.front.load(std::sync::atomic::Ordering::SeqCst) }
     }
-    fn back_mut(&self) -> &mut FrameData {
-        unsafe { &mut *self.back.load(std::sync::atomic::Ordering::SeqCst) }
+    fn temp_mut(&self) -> &mut FrameData {
+        unsafe { &mut *self.temp.load(std::sync::atomic::Ordering::SeqCst) }
+    }
+    fn temp_to_back(&self) {
+        let back_ptr = self.back.load(std::sync::atomic::Ordering::SeqCst);
+        let temp_ptr = self
+            .temp
+            .swap(back_ptr, std::sync::atomic::Ordering::SeqCst);
+        self.back
+            .store(temp_ptr, std::sync::atomic::Ordering::SeqCst);
     }
 }
 
@@ -66,9 +78,11 @@ impl Drop for DoubleBuffer {
     fn drop(&mut self) {
         let front_ptr = self.front.load(std::sync::atomic::Ordering::SeqCst);
         let back_ptr = self.back.load(std::sync::atomic::Ordering::SeqCst);
+        let temp_ptr = self.temp.load(std::sync::atomic::Ordering::SeqCst);
         unsafe {
             drop(Box::from_raw(front_ptr));
             drop(Box::from_raw(back_ptr));
+            drop(Box::from_raw(temp_ptr));
         }
     }
 }
@@ -93,7 +107,7 @@ fn preload_images(paths: &[PathBuf]) -> Vec<image::DynamicImage> {
 }
 
 fn main() {
-    let size = 10;
+    let size = 5;
     let mut stdout = std::io::stdout();
     enable_raw_mode().unwrap();
     execute!(
@@ -113,57 +127,73 @@ fn main() {
         for path in paths.iter() {
             //for img in images.iter() {
             let img = image::open(&path).unwrap();
+            // new_request_rx.recv().unwrap();
+            let back = db_cpu.temp_mut();
+            {
+                let cols = back.width;
+                let rows = back.height;
+                let chunk_rows = rows / 24;
+                let blocks = (0..24)
+                    .into_par_iter()
+                    .map(|block_id| {
+                        let start = block_id * chunk_rows;
+                        let end = start + chunk_rows;
+                        let mut buf = Vec::with_capacity(((end - start) * cols) as usize * 20);
+                        for y in start..end {
+                            for x in 0..cols {
+                                let [r, g, b, _] = img.get_pixel(x, y).0;
+                                write!(
+                                    &mut buf,
+                                    "\x1b[{};{}H\x1b[38;2;{};{};{}m{}",
+                                    y,
+                                    x * 2,
+                                    r,
+                                    g,
+                                    b,
+                                    "██"
+                                )
+                                .unwrap();
+                            }
+                        }
+                        buf
+                    })
+                    .collect::<Vec<Vec<u8>>>();
+
+                back.data.clear();
+                for row in blocks {
+                    back.data.extend(row);
+                }
+            }
             new_request_rx.recv().unwrap();
-            let cols = img.width();
-            let back = db_cpu.back_mut();
-            let slice = &mut back.data;
-            slice
-                .par_chunks_mut(back.width as usize)
-                .enumerate()
-                .for_each(|(y, row)| {
-                    for x in 0..cols {
-                        let pixel = img.get_pixel(x, y as u32);
-                        let [r, g, b, a] = pixel.0;
-                        let c = if a == 0 { ' ' } else { '■' };
-                        row[x as usize] = (r, g, b, c);
-                    }
-                });
+            db_cpu.temp_to_back();
             frame_ready_tx.send(()).unwrap();
         }
     });
     let db_render = Arc::clone(&double_buffer);
     let render_handle = thread::spawn(move || {
         new_request_tx.send(()).unwrap();
-        let mut write_buffer =
-            Vec::with_capacity((db_render.front().width * db_render.front().height * 20) as usize);
+        // let mut write_buffer =
+        //     Vec::with_capacity((db_render.front().width * db_render.front().height * 20) as usize);
         let mut stdout = std::io::stdout();
+        let frame_time = std::time::Duration::from_millis(1000 / 16);
+        let mut delay = std::time::Duration::ZERO;
         loop {
-            execute!(stdout, MoveTo(0, 0)).unwrap();
             let now = std::time::Instant::now();
             frame_ready_rx.recv().unwrap();
             {
                 let front = db_render.front();
-                for y in 0..front.height {
-                    for x in 0..front.width {
-                        let (r, g, b, c) = front.data[(y * front.width + x) as usize];
-                        write!(
-                            &mut write_buffer,
-                            "\x1b[{};{}H\x1b[38;2;{};{};{}m{}",
-                            y, x, r, g, b, c
-                        )
-                        .unwrap();
-                    }
-                }
+                stdout.write_all(&front.data).unwrap();
             }
             // Reset the cursor position
             execute!(stdout, crossterm::cursor::MoveTo(0, 0)).unwrap();
-            let _ = stdout.write_all(&write_buffer);
-            write_buffer.clear();
             let elapsed = now.elapsed();
+            delay += elapsed;
             // 16 FPS
-            let target_frame_time = std::time::Duration::from_millis(1000 / 16);
-            if elapsed < target_frame_time {
-                thread::sleep(target_frame_time - elapsed);
+            if delay < frame_time {
+                thread::sleep(frame_time - delay);
+                delay = std::time::Duration::ZERO;
+            } else {
+                delay -= frame_time;
             }
             // Swap the buffers
             double_buffer.swap();
